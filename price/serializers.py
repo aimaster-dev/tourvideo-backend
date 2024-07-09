97132f0196c8ef883ab66bcc7ed83d025f396b38
cleverpanther <cleverpanther0819@gmail.com>
2024-07-09 11:21:52 -0700

    price update

--- price/serializers.py
+++ price/serializers.py
@@ -5,9 +5,10 @@ class PriceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Price
-        fields = ["id", "level", "title", "record_time", "record_limit", "created_at", "updated_at"]
+        fields = ["id", "price", "level", "title", "record_time", "record_limit", "created_at", "updated_at"]
 
     def update(self, instance, validated_data):
+        instance.price = validated_data.get('price', instance.price)
         instance.level = validated_data.get('level', instance.level)
         instance.title = validated_data.get('title', instance.title)
         instance.record_time = validated_data.get('record_time', instance.record_time)
